c7d7d67c72355d672584d62be378f86aa52bb315
Yulai202020 <ngmyulai@gmail.com>
2025-05-01 11:26:43 +0400

    added many thinks

--- main.c
+++ main.c
@@ -22,13 +22,53 @@ json_object *jarray = NULL;
 sqlite3* db = NULL;
 table_t* table;
 
+char* dns_filters = NULL;
 int max_count = -1;
 int passed_count = 0;
+int port = 53;
+
+typedef struct {
+    int type;
+    int class;
+} DNS_Data;
 
 char* itos(int value) {
     char* str = malloc(12);
     if (str) sprintf(str, "%d", value);
     return str;
+}
+
+char** split_by_space(char* input, int* count) {
+    char* temp = strdup(input);  // Duplicate input so original is unchanged
+    char* token = strtok(temp, " ");
+    int capacity = 10;
+    int size = 0;
+
+    char** result = malloc(capacity * sizeof(char*));
+    if (!result) return NULL;
+
+    while (token) {
+        if (size >= capacity) {
+            capacity *= 2;
+            result = realloc(result, capacity * sizeof(char*));
+        }
+        result[size++] = strdup(token);  // Copy token
+        token = strtok(NULL, " ");
+    }
+
+    free(temp);  // Free the duplicated string
+    *count = size;
+    return result;
+}
+
+bool ends_with(const char *str, const char *suffix) {
+    if (!str || !suffix)
+        return false;
+    size_t len_str = strlen(str);
+    size_t len_suffix = strlen(suffix);
+    if (len_suffix > len_str)
+        return false;
+    return strncmp(str + len_str - len_suffix, suffix, len_suffix) == 0;
 }
 
 int bytesToInt(int count, ...) {
@@ -101,7 +141,36 @@ int writeJsonToFile(const char* filename, json_object* jobj) {
     return 0;
 }
 
+bool isEqual(char* s1, char* s2) {
+    return strcmp(s1, s2) == 0;
+}
+
+int match_filter(DNS_Data item, const char* filter) {
+    char buffer[256];
+    strcpy(buffer, filter);
+
+    char* token = strtok(buffer, " ");
+    while (token) {
+        char* key = token;
+        token = strtok(NULL, " ");
+        if (!token) break;
+
+        char* value = token;
+        token = strtok(NULL, " ");
+
+        if (strcmp(key, "type") == 0 && item.type == atoi(value)) {
+            return 0;
+        } else if (strcmp(key, "class") == 0 && item.class == atoi(value)) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
 void packet_handler(unsigned char* user, const pcap_pkthdr* header, const unsigned char* packet) {
+    size_t len = header->len;
+
     if (passed_count >= max_count && max_count != -1) {
         pcap_breakloop(handle);
         return;
@@ -137,17 +206,80 @@ void packet_handler(unsigned char* user, const pcap_pkthdr* header, const unsign
     }
 
     // check is dns
-    if (!(protocol == 17 && (srcPort == 53 || dstPort == 53))) {
+    if (!(protocol == 17 && (srcPort == port || dstPort == port))) {
         return;
     }
+
+    // body
+
+    int id = bytesToInt(2, packet[start], packet[start + 1]);
+
+    start+=2;
+
+    int qr = packet[start] & 0x10000000;
+    int opcode = packet[start] & 0x01111000;
+    int aa = packet[start] & 0x00000100;
+    int tc = packet[start] & 0x00000010;
+    int rd = packet[start] & 0x00000001;
+
+    start+=1;
+
+    int ra = packet[start] & 0x10000000;
+    int rcode = packet[start] & 0x00001111;
+
+    start+=1;
+
+    int qdcount = bytesToInt(2, packet[start], packet[start + 1]);
+    int ancount = bytesToInt(2, packet[start + 2], packet[start + 3]);
+    int nscount = bytesToInt(2, packet[start + 4], packet[start + 5]);
+    int arcount = bytesToInt(2, packet[start + 6], packet[start + 7]);
+
+    start+=8;
+
+    DNS_Data data[qdcount];
+
+    // questions
+    for (int i = 0; i < qdcount; i++) {
+        unsigned char tmp = packet[start];
+
+        while (tmp != NULL) {
+            start += 1;
+            tmp = packet[start];
+        }
+
+        start += 1; // skip name
+
+        int qtype = bytesToInt(2, packet[start], packet[start+1]);
+        int qclass = bytesToInt(2, packet[start+2], packet[start+3]);
+
+        start += 4;
+
+        data[i].type = qtype;
+        data[i].class = qclass;
+
+        if (dns_filters && !match_filter(data[i], dns_filters)) {
+            return;
+        }
+    }
+
+    // answers
+    for (int i = 0; i < ancount; i++) {
+        start += 10;
+        int datalen = bytesToInt(2, packet[start], packet[start + 1]);
+        start += datalen;
+    }
+
+    for (int i = 0; i < nscount; i++) {}
+
+    for (int i = 0; i < arcount; i++) {}
 
     // print table
     print_middle(table);
 
     if (srcPort > 0 && dstPort > 0) {
-        print_ln(table, itos(header->len), srcIP, itos(srcPort), dstIP, itos(dstPort));
+        print_ln(table, itos(header->len), srcIP, itos(srcPort), dstIP, itos(dstPort), itos(id), itos(qdcount), itos(ancount));
     } else {
-        print_ln(table, itos(header->len), srcIP, "", dstIP, "");
+        print_ln(table, itos(header->len), srcIP, "", dstIP, "", itos(id), itos(qdcount), itos(ancount));
     }
 
     // dumping
@@ -161,25 +293,15 @@ void packet_handler(unsigned char* user, const pcap_pkthdr* header, const unsign
         json_object_object_add(tmp, "srcPort", json_object_new_int(srcPort));
         json_object_object_add(tmp, "dstIP", json_object_new_string(dstIP));
         json_object_object_add(tmp, "dstPort", json_object_new_int(dstPort));
+        json_object_object_add(tmp, "id", json_object_new_int(id));
+        json_object_object_add(tmp, "question_count", json_object_new_int(qdcount));
+        json_object_object_add(tmp, "answer_count", json_object_new_int(ancount));
         json_object_array_add(jarray, tmp);
     } if (db) {
         char *sql;
         sprintf(sql, "INSERT INTO data (packet_length, srcIP, srcPort, dstIP, dstPort) VALUES ('%d', '%s', '%d', '%s', '%d');", header->len, srcIP, srcPort, dstIP, dstPort);
         sqlite3_exec(db, sql, 0, 0, NULL);
     }
-
-    // body
-
-    int qr = packet[start] & 0x10000000;
-    int opcode = packet[start] & 0x01111000;
-    int aa = packet[start] & 0x00000100;
-    int tc = packet[start] & 0x00000010;
-    int rd = packet[start] & 0x00000001;
-
-    start+=1;
-
-    int ra = packet[start] & 0x10000000;
-    int rcode = packet[start] & 0x00001111;
 
     passed_count++;
 }
@@ -196,10 +318,16 @@ int main(int argc, char* argv[]) {
     int length = 0;
     int opt;
 
-    while ((opt = getopt(argc, argv, "s:w:c:f:d:j:b:r:")) != -1) {
+    while ((opt = getopt(argc, argv, "w:s:c:f:d:a:p:h")) != -1) {
         switch (opt) {
             case 'w':
-                output = optarg;
+                if (ends_with(output, ".json") || ends_with(output, ".jsonl")) {
+                    json_file = optarg;
+                } else if (ends_with(output, ".db")) {
+                    db_file = optarg;
+                } else { // pcap
+                    output = optarg;
+                } 
                 break;
             case 's':
                 length = atoi(optarg);
@@ -213,14 +341,27 @@ int main(int argc, char* argv[]) {
             case 'd':
                 config_file = optarg;
                 break;
-            case 'j':
-                json_file = optarg;
+            case 'a':
+                dns_filters = optarg;
                 break;
-            case 'b':
-                db_file = optarg;
+            case 'p':
+                port = atoi(optarg);
+                break;
+            case 'h':
+                // print help
+                printf("Usage: %s [options]\n\n", argv[0]);
+                printf("Program to capture dns packets\n\n");
+                printf("Options:\n");
+                printf("-w  Write captured data to file\n");
+                printf("-c  Sets max count of captured packets\n");
+                printf("-f  Sets filters\n");
+                printf("-d  Get data from config file\n");
+                printf("-a  Sets dns filters\n");
+                printf("-p  Sets dns port\n");
+                return 0;
                 break;
             default:
-                printf("Unknown error\n");
+                return 1;
         }
     }
 
@@ -246,8 +387,12 @@ int main(int argc, char* argv[]) {
         if (json) {
             json_object* filter_obj;
             json_object* count_obj;
-            json_object* length_obj;
+            json_object* dns_filter_obj;
             json_object* output_obj;
+
+            if (json_object_object_get_ex(json, "dns_filter", &dns_filter_obj)) {
+                dns_filters = json_object_get_string(dns_filter_obj);
+            }
 
             if (json_object_object_get_ex(json, "filter", &filter_obj)) {
                 filter_exp = json_object_get_string(filter_obj);
@@ -284,7 +429,7 @@ int main(int argc, char* argv[]) {
 
         pcap_freecode(&fp);
     } if (output) {
-        dumper = pcap_dump_open(handle, "capture.pcap");
+        dumper = pcap_dump_open(handle, output);
         if (!dumper) {
             fprintf(stderr, "Error opening output file: %s\n", pcap_geterr(handle));
             return 1;
@@ -314,12 +459,12 @@ int main(int argc, char* argv[]) {
     }
 
     // init table
-    int widths[] = {13, 9, 11, 14, 16};
+    int widths[] = {13, 9, 11, 14, 16, 5, 18, 16};
 
-    table = init_table(5, widths, 1);
+    table = init_table(8, widths, 1);
 
     print_start(table);
-    print_ln(table, "Packet Length", "source ip", "source port", "destination ip", "destination port");
+    print_ln(table, "Packet Length", "source ip", "source port", "destination ip", "destination port", "ID", "Count of Questions", "Count of Answers");
 
     if (pcap_loop(handle, 0, packet_handler, (unsigned char *)dumper) == -1) {
         fprintf(stderr, "Error capturing packets: %s\n", pcap_geterr(handle));
